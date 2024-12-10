56f48357eecd79add94052ca3e17972c939269ee
NurullayevJalolbek <nurullayevjalolbek28@gmail.com>
2024-12-10 22:14:54 +0500

    install api

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::post('/nimadir', function (Request $request) {
+    return $request->all();
+});
